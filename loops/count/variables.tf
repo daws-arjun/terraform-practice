7da97a1683a774143b2c95aeafbe35cf55b5e7f2
daws-arjun <a.nagarjuna97@gmail.com>
2025-10-14 22:15:30 +0530

    latest

---
+++ loops/count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z06547222JC0EDVX2ID1B"
+}
+
+variable "domain_name" {
+    default = "anuarjun.space"
+}
